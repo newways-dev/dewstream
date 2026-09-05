05f55f65fc0aaa38baeaca97786ddb27251bb036
Kate <kate.dew.next@gmail.com>
2026-09-05 20:48:25 +0200

    add s3 storage

--- dewstream-backend/src/core/core.module.ts
+++ dewstream-backend/src/core/core.module.ts
@@ -10,6 +10,7 @@ import { TotpModule } from '@/modules/auth/totp/totp.module'
 import { VerificationModule } from '@/modules/auth/verification/verification.module'
 import { CronModule } from '@/modules/cron/cron.module'
 import { MailModule } from '@/modules/libs/mail/mail.module'
+import { StorageModule } from '@/modules/libs/storage/storage.module'
 
 import { AccountModule } from '../modules/auth/account/account.module'
 import { IS_DEV_ENV } from '../shared/utils/is-dev.util'
@@ -40,7 +41,8 @@ import { RedisModule } from './redis/redis.module'
 		PasswordRecoveryModule,
 		TotpModule,
 		DeactivateModule,
-		CronModule
+		CronModule,
+		StorageModule
 	]
 })
 export class CoreModule {}
