a5d88adc094b30d99bc4e0d2753376a32d825a36
Kate <kate.dew.next@gmail.com>
2026-09-05 20:23:20 +0200

    change email and password

--- dewstream-backend/src/modules/auth/account/account.service.ts
+++ dewstream-backend/src/modules/auth/account/account.service.ts
@@ -6,9 +6,12 @@ import {
 import { hash, verify } from 'argon2'
 
 import { PrismaService } from '@/core/prisma/prisma.service'
+import { User } from '@/generated/prisma/client'
 
 import { VerificationService } from '../verification/verification.service'
 
+import { ChangeEmailInput } from './inputs/change-email.input'
+import { ChangePasswordInput } from './inputs/change-password.input'
 import { CreateUserInput } from './inputs/create-user.input'
 
 @Injectable()
@@ -61,6 +64,42 @@ export class AccountService {
 		})
 
 		await this.verificationService.sendVerificationToken(user)
+
+		return true
+	}
+
+	public async changeEmail(user: User, input: ChangeEmailInput) {
+		const { email } = input
+
+		await this.prismaService.user.update({
+			where: {
+				id: user.id
+			},
+			data: {
+				email
+			}
+		})
+
+		return true
+	}
+
+	public async changePassword(user: User, input: ChangePasswordInput) {
+		const { oldPassword, newPassword } = input
+
+		const isValidPassword = await verify(user.password, oldPassword)
+
+		if (!isValidPassword) {
+			throw new UnauthorizedException('Old password is incorrect')
+		}
+
+		await this.prismaService.user.update({
+			where: {
+				id: user.id
+			},
+			data: {
+				password: await hash(newPassword)
+			}
+		})
 
 		return true
 	}
